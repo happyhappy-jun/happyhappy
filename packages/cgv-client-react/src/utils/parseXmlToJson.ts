0e195a0b8b1a77c1a22d41f4345db6122215deef
unajun <bjyoon513@gmail.com>
2022-12-30 23:09:34 +0900

    fix: change type of parsexml util func

--- packages/cgv-client-react/src/utils/parseXmlToJson.ts
+++ packages/cgv-client-react/src/utils/parseXmlToJson.ts
@@ -1,8 +1,8 @@
-import {XMLParser} from 'fast-xml-parser';
+import { XMLParser } from "fast-xml-parser";
 
-function parseXmlToJson<Type>(xml: string | undefined): Type {
-    const parser = new XMLParser();
-    return parser.parse(xml) as Type;
+function parseXmlToJson<Type>(xml: string): Type {
+  const parser = new XMLParser();
+  return parser.parse(xml) as Type;
 }
 
 export default parseXmlToJson;
